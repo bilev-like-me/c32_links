96b2408e2d22c01eac8e507a7b80dfac07564fad
Aleksandr Bilev <bilevspb@gmail.com>
2021-09-08 23:10:52 +0300

    wip

--- links/settings.py
+++ links/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+from .secrets import *
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-qqm7tzbiwl11o@brkh)h8io^-%%$5)5ol_$ng%e_^$r0!^g1o)'
+# SECRET_KEY = 'django-insecure-qqm7tzbiwl11o@brkh)h8io^-%%$5)5ol_$ng%e_^$r0!^g1o)'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
