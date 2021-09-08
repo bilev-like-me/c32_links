c5b3abba01dd8f9cf760cf1353221b786a89cedd
Aleksandr Bilev <bilevspb@gmail.com>
2021-09-08 22:53:37 +0300

    wip

--- short_links/models.py
+++ short_links/models.py
@@ -1,3 +1,6 @@
 from django.db import models
+from hashid_field import HashidAutoField
 
-# Create your models here.
+class ShortLinks(models.Model):
+    id = HashidAutoField(primary_key=True)
+    long_link = models.CharField(max_length=2048)
--- short_links/views.py
+++ short_links/views.py
@@ -4,3 +4,7 @@ from django.views import View
 class MainView(View):
     def get(self, request):
         return render(request, "index.html")
+    
+    def post(self, request):
+        print(request.POST['link'])
+        return render(request, "index.html")
