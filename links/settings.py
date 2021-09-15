5abfe196013ac37f4b4f51caf08aa5a8cd898244
Aleksandr Bilev <bilevspb@gmail.com>
2021-09-15 14:09:36 +0300

    final

--- links/settings.py
+++ links/settings.py
@@ -12,7 +12,6 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 from my_secrets import *
-import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -25,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECRET_KEY = 'django-insecure-qqm7tzbiwl11o@brkh)h8io^-%%$5)5ol_$ng%e_^$r0!^g1o)'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['127.0.0.1']
 
@@ -123,9 +122,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
-STATIC_DIR = os.path.join(BASE_DIR, 'static')
-# STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+]
+STATIC_DIR = BASE_DIR / "static"
+# STATIC_ROOT = BASE_DIR / "static"
 
 
 # Default primary key field type
